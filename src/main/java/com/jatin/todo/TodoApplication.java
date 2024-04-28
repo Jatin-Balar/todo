718e56af9bd3cfbe3d8a94427a05202a5cef6d03
Sara Patel <jatinbalar348@gmail.com>
2024-04-28 09:25:48 -0400

    first commit

--- src/main/java/com/jatin/todo/TodoApplication.java
+++ src/main/java/com/jatin/todo/TodoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -16,6 +19,17 @@ public class TodoApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(TodoApplication.class);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedMethods("*")
+					.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
